179cd3862325b3b109b7fff8bf29a8c9fbb5a5e7
somnath <somnathmishra100dbi@gmail.com>
2025-02-13 22:35:17 +0530

    added5

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://edu-backend.devs24.com",
         changeOrigin: true,
       },
     },
